33aa80c204c7d326cfe425f3f6973211a3979edb
unclechao <uncle.chao@live.cn>
2020-04-06 15:38:05 +0800

    title动态显示

--- pages/mes/index.vue
+++ pages/mes/index.vue
@@ -2,13 +2,13 @@
 	<view>
 		<!-- 页眉 -->
 		<cu-custom bgColor="bg-black">
-			<block slot="content">MES 管理系统</block>
+			<block slot="content">{{title}}</block>
 		</cu-custom>
 		<!-- 内容 -->
 		<scroll-view v-show="PageCur=='index'" scroll-y class="page">
 			<image src="/static/rochiev.png" mode="widthFix" style="padding:20upx" class="response"></image>
 			<view class="nav-list">
-				<view @click="ComponentChange(item.name)" class="nav-li" :class="'bg-'+item.color" :style="[{animation: 'show ' + ((index+1)*0.2+1) + 's 1'}]"
+				<view @click="ComponentChange(item)" class="nav-li" :class="'bg-'+item.color" :style="[{animation: 'show ' + ((index+1)*0.2+1) + 's 1'}]"
 				 v-for="(item,index) in elements" :key="index">
 					<view class="nav-title">{{item.title}}</view>
 					<view class="nav-name">{{item.name}}</view>
@@ -51,6 +51,7 @@
 	export default {
 		data() {
 			return {
+				title: 'MES 管理系统',
 				PageCur: 'index',
 				elements: [{
 						title: '工单',
@@ -82,9 +83,27 @@
 		methods: {
 			NavChange: function(e) {
 				this.PageCur = e.currentTarget.dataset.cur
+				switch (this.PageCur) {
+					case "device":
+						this.title = '设备'
+						break;
+					case "my":
+						this.title = '我的'
+						break;
+					case "help":
+						this.title = '帮助'
+						break;
+					case "workOrder":
+						this.title = '工单'
+						break;
+					default:
+						this.title = 'MES 管理系统'
+						break;
+				}
 			},
-			ComponentChange: function(pageName) {
-				this.PageCur = pageName
+			ComponentChange: function(page) {
+				this.PageCur = page.name
+				this.title = page.title
 			}
 		}
 	}
